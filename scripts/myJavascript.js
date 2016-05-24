daca84db42cc8971099ce43bef833a3850f54110
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-05-24 18:32:25 -0500

    First commit

---
+++ scripts/myJavascript.js
@@ -0,0 +1,20 @@
+console.log("Hola!");
+
+
+var supremeAwesome = function() {
+var pupsVisibility = document.getElementById("pupsKennel").style.visibility;
+if (pupsVisibility == "hidden") {
+document.getElementById("pupsKennel").style.visibility ="visible";
+}
+
+else if (pupsVisibility == "visible"){
+  document.getElementById("pupsKennel").style.visibility ="hidden";
+
+}
+
+else {
+  document.getElementById("pupsKennel").style.visibility ="visible";
+
+}
+
+};
